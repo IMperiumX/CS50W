a18afaed27d5eaaa8620355c958725542098649a
yusufadell <youseefadel777@gmail.com>
2021-12-29 01:36:00 +0200

    allow all hosts in settings

--- 1-wiki/wiki/settings.py
+++ 1-wiki/wiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
