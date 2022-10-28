c3df7aa88e792327b8895c6b7a5f7836ba4b9556
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-10-28 17:37:27 +0200

    add send_email function to utils

--- 1-wiki/encyclopedia/util.py
+++ 1-wiki/encyclopedia/util.py
@@ -2,6 +2,8 @@ import re
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def list_entries():
@@ -40,3 +42,20 @@ def get_entry(title):
         return f.read().decode("utf-8")
     except FileNotFoundError:
         return "## Page was not found"
+
+
+
+def send_email(request):
+    subject = request.POST.get('subject', '')
+    message = request.POST.get('message', '')
+    from_email = request.POST.get('from_email', '')
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, from_email, ['admin@example.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return HttpResponseRedirect('/contact/thanks/')
+    else:
+        # In reality we'd use a form class
+        # to get proper validation errors.
+        return HttpResponse('Make sure all fields are entered and valid.')
