48c26530ae34dd17a823d874e2696741feddaefd
yusufadell <yusufadell.dev@gmail.com>
2023-02-02 13:40:48 +0200

    added django-silk for performance profiling

--- 2-commerce/commerce/urls.py
+++ 2-commerce/commerce/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("auctions.urls"))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("auctions.urls"))]
+
+urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
